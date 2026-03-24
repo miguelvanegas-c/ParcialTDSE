36c65f02454c3f16a9de35f06ba98c8a4c62339c
unknown <miguel.vanegas-c@labinfo.is.escuelaing.edu.co>
2026-03-24 10:18:48 -0500

    finish CatalanService

--- src/main/java/com/example/demo/CatalanService.java
+++ src/main/java/com/example/demo/CatalanService.java
@@ -1,11 +1,25 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Indexed;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CatalanService {
 
     public CatalanResponse catalan(int num){
-        int
+        int c0 = 1;
+        List<Integer> list = new ArrayList<>();
+        list.add(c0);
+        for (int i = 1; i< num+1; i++){
+            int result = 0;
+            for(int n = 0; n < i; n++){
+                result += list.get(n) * list.get(i-1-n);
+            }
+            list.add(result);
+        }
+        return new CatalanResponse("Secuencia de Catalan", num,list);
     }
 }
